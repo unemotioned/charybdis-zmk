c30a184f0511704fcb8f3c3e23f6450357f6a67d
unemotioned <blackeagle10@icloud.com>
2026-05-15 12:02:41 +0900

    Add: .editorconfig - format .sh with 4 space indent

--- build.sh
+++ build.sh
@@ -25,50 +25,50 @@ cd "$ROOT_DIR"
 SECONDS=0
 
 build_target() {
-  local build_dir="$1"
-  local shield="$2"
-  local controller="$3"
+    local build_dir="$1"
+    local shield="$2"
+    local controller="$3"
 
-  # NOTE: Add the following option in "$ west build" before the "--" to build cleanly
-  # -p always \
-  west build \
-    -d "$build_dir" \
-    -b "$controller" \
-    -s zmk/app \
-    -- \
-    -DSHIELD="$shield" \
-    -DZMK_CONFIG="$ROOT_DIR/config" \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+    # NOTE: Add the following option in "$ west build" before the "--" to build cleanly
+    # -p always \
+    west build \
+        -d "$build_dir" \
+        -b "$controller" \
+        -s zmk/app \
+        -- \
+        -DSHIELD="$shield" \
+        -DZMK_CONFIG="$ROOT_DIR/config" \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 }
 
 build_target build/left "$shield_left" "$controller"
 build_target build/right "$shield_right" "$controller"
 
 if [ "$build_reset" = true ]; then
-  build_target build/settings_reset settings_reset "$controller"
+    build_target build/settings_reset settings_reset "$controller"
 fi
 
 mkdir -p output/bak
 
 # backup to bak/
 [ -f output/"$shield_left".uf2 ] &&
-  mv output/"$shield_left".uf2 output/bak
+    mv output/"$shield_left".uf2 output/bak
 
 [ -f output/"$shield_right".uf2 ] &&
-  mv output/"$shield_right".uf2 output/bak
+    mv output/"$shield_right".uf2 output/bak
 
 [ -f output/settings_reset.uf2 ] &&
-  mv output/settings_reset.uf2 output/bak
+    mv output/settings_reset.uf2 output/bak
 
 # copy the built uf2 to output/
 [ -f build/left/zephyr/zmk.uf2 ] &&
-  cp build/left/zephyr/zmk.uf2 output/"$shield_left".uf2
+    cp build/left/zephyr/zmk.uf2 output/"$shield_left".uf2
 
-[ -f build/right/zephyr/zmk.uf2 ]  &&
-  cp build/right/zephyr/zmk.uf2 output/"$shield_right".uf2
+[ -f build/right/zephyr/zmk.uf2 ] &&
+    cp build/right/zephyr/zmk.uf2 output/"$shield_right".uf2
 
 [[ "$build_reset" = true && -f build/settings_reset/zephyr/zmk.uf2 ]] &&
-  cp build/settings_reset/zephyr/zmk.uf2 output/settings_reset.uf2
+    cp build/settings_reset/zephyr/zmk.uf2 output/settings_reset.uf2
 
 echo -e "\n----------------------------------------------"
 echo -e "\n Build done. (took ${SECONDS}s)"
@@ -77,5 +77,5 @@ echo -e "\n----------------------------------------------\n"
 
 read -rp 'Open output directory with finder? [Y/n]: ' answer
 if [[ -z "${answer,,}" || "${answer,,}" == 'y' ]]; then
-  open ./output
+    open ./output
 fi
